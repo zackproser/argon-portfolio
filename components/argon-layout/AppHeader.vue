4f965f5bc693ef1ec8594b95693d873189c73240
Zack Proser <zproser@cloudflare.com>
2019-10-29 00:03:06 -0700

    Social links

--- components/argon-layout/AppHeader.vue
+++ components/argon-layout/AppHeader.vue
@@ -60,29 +60,22 @@
             </ul>
             <ul class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.facebook.com/creativetim" target="_blank" rel="noopener"
-                       data-toggle="tooltip" title="Like us on Facebook">
-                        <i class="fab fa-facebook-square"></i>
-                        <span class="nav-link-inner--text d-lg-none">Facebook</span>
-                    </a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link nav-link-icon" href="https://www.instagram.com/zackproser"
-                       target="_blank" rel="noopener" data-toggle="tooltip" title="Follow us on Instagram">
+                       target="_blank" rel="noopener" data-toggle="tooltip" title="View my artwork">
                         <i class="fab fa-instagram"></i>
                         <span class="nav-link-inner--text d-lg-none">Instagram</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://twitter.com/creativetim" target="_blank" rel="noopener"
-                       data-toggle="tooltip" title="Follow us on Twitter">
+                    <a class="nav-link nav-link-icon" href="https://twitter.com/zackproser" target="_blank" rel="noopener"
+                       data-toggle="tooltip" title="Follow me on Twitter">
                         <i class="fab fa-twitter-square"></i>
                         <span class="nav-link-inner--text d-lg-none">Twitter</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://github.com/creativetimofficial/vue-argon-design-system"
-                       target="_blank" rel="noopener" data-toggle="tooltip" title="Star us on Github">
+                    <a class="nav-link nav-link-icon" href="https://github.com/zackproser"
+                       target="_blank" rel="noopener" data-toggle="tooltip" title="Find me on Github">
                         <i class="fab fa-github"></i>
                         <span class="nav-link-inner--text d-lg-none">Github</span>
                     </a>
