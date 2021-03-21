1a93b24577eff3722ad1309a79f04b85ca7cc024
Zack Proser <zackproser@gmail.com>
2021-03-21 18:48:32 -0400

    March 2021 update

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,6 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' }
     ]
   },
 
