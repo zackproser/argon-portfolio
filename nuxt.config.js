d896ce27287b75930c7ed6bdf94a2470b22cdc43
Zachary Proser <zackproser@gmail.com>
2021-04-25 13:23:18 -0400

    Fix mobile navbar dismissal. Prettify config

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,31 +18,33 @@ var dynamicPaths = getDynamicRoutes()
 
 module.exports = {
   generate: {
-    routes: dynamicPaths
+    routes: dynamicPaths,
   },
   mode: 'universal',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'X-UA-Compatible', content: 'IE=edge' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.5, user-scalable=1, shrink-to-fit=no' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.5, user-scalable=1, shrink-to-fit=no',
+      },
       { hid: 'description', name: 'description', content: pkg.description },
-      { name: 'author', content: 'Zack Proser' }
+      { name: 'author', content: 'Zack Proser' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   /*
-  ** Configuration for @nuxtjs/pwa
-  ** https://developer.mozilla.org/en-US/docs/Web/Manifest
-  */
+   ** Configuration for @nuxtjs/pwa
+   ** https://developer.mozilla.org/en-US/docs/Web/Manifest
+   */
   manifest: {
     name: 'Zack Proser Portfolio',
     short_name: 'Zack Proser',
@@ -54,74 +56,71 @@ module.exports = {
     // apple-mobile-web-app-capable=yes
     // https://medium.com/@firt/dont-use-ios-web-app-meta-tag-irresponsibly-in-your-progressive-web-apps-85d70f4438cb
     mobileAppIOS: true,
-    appleStatusBarStyle: '#172b4d'
+    appleStatusBarStyle: '#172b4d',
   },
 
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
     '~assets/argon/vendor/nucleo/css/nucleo.css',
     '@fortawesome/fontawesome-free/css/all.css',
     '~assets/argon/scss/argon.scss',
     'bootstrap-vue/dist/bootstrap-vue.css',
-    '~assets/transitions.css'
+    '~assets/transitions.css',
   ],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~/plugins/argon/argon-kit',
-    '~/plugins/common.js'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['~/plugins/argon/argon-kit', '~/plugins/common.js'],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
-    ['bootstrap-vue/nuxt', {
-      bootstrapCSS: false,
-      bootstrapVueCSS: false,
-      componentPlugins: [
-        'Carousel',
-        'Spinner'
-      ],
-      directivePlugins: [
-        'Tooltip',
-        'Popover'
-      ]
-    }],
-    '@nuxtjs/pwa'
+    [
+      'bootstrap-vue/nuxt',
+      {
+        bootstrapCSS: false,
+        bootstrapVueCSS: false,
+        componentPlugins: ['Carousel', 'Spinner'],
+        directivePlugins: ['Tooltip', 'Popover'],
+      },
+    ],
+    '@nuxtjs/pwa',
   ],
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     extend(config, ctx) {
       config.module.rules.push({
         test: /\.md$/,
-        include: [path.resolve(__dirname, 'posts'), path.resolve(__dirname, 'blog')],
-        loader: 'frontmatter-markdown-loader'
+        include: [
+          path.resolve(__dirname, 'posts'),
+          path.resolve(__dirname, 'blog'),
+        ],
+        loader: 'frontmatter-markdown-loader',
       })
-    }
-  }
+    },
+  },
 }
--- plugins/common.js
+++ plugins/common.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 
 Vue.mixin({
-    methods: {
-        getClassType () {
-            let c = [
-                'default',
-                'primary',
-                'info',
-                'success',
-                'danger',
-                'warning'
-            ]
-            return c[Math.floor(Math.random() * c.length)]
-        }
-    }
+  methods: {
+    getClassType() {
+      let c = ['default', 'primary', 'info', 'success', 'danger', 'warning']
+      return c[Math.floor(Math.random() * c.length)]
+    },
+  },
+  mounted() {
+    let element = document.querySelector('.navbar-collapse')
+    element.classList.remove('show')
+  },
 })
