3b89df23df7ae80287ac0fe7ffdaca9820d2f998
Zachary Proser <zackproser@gmail.com>
2023-01-30 12:06:53 +0000

    Test commit

--- plugins/common.js
+++ plugins/common.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 Vue.mixin({
   methods: {
     getClassType() {
+      // This does some things
       let c = ['default', 'primary', 'info', 'success', 'danger', 'warning']
       return c[Math.floor(Math.random() * c.length)]
     },
